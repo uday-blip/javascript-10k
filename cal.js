dd3a29b02f4e7775c37410da135b137a1d374d4e
uday doddavarapu <udayudayharry@gmail.com>
2025-07-21 16:40:27 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,29 @@
+let button =document.getElementsByClassName("btn");
+let screen=document.getElementById("screen");
+
+
+text="";
+for (let i of button){
+     i.onclick=()=>{
+        if(i.innerHTML=="1" || i.innerHTML=="2"||i.innerHTML=="3"||i.innerHTML=="4"||i.innerHTML=="5"||i.innerHTML=="6"||i.innerHTML=="7"||i.innerHTML=="8"||i.innerHTML=="9"||i.innerHTML=="0"){
+            text+=i.innerHTML
+            screen.innerHTML=text
+        }else if(i.innerHTML=="+" || i.innerHTML=="-" ||i.innerHTML=="*" ||i.innerHTML=="/" ||i.innerHTML=="." ){
+            if("+-*/".includes(text[text.length-1])){
+                text=text.slice(0,text.length-1)
+            }
+            text+=i.innerHTML
+            screen.innerHTML=text;
+        }else if(i.innerHTML == "c"){
+            text="0"
+            screen.innerHTML=text;
+        }else if(i.innerHTML == "x"){
+              text = text.slice(0,text.length-1);
+              screen.innerHTML=text;    
+        }else if(i.innerHTML == "="){
+            text =eval(text);
+            screen.innerHTML=text;
+        }
+     }
+     }
+
